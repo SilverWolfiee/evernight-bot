f3b4e234ae3a793903c1da090a0f84ff3f7dff2e
SilverWolf <rblenderdrag@gmail.com>
2025-10-27 23:33:03 +0700

    :>>>>>>

--- src/commands/profile.js
+++ src/commands/profile.js
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } from "discord.js";
 import { loadUsers } from "../../data/userdata.js";
 
 export const command = new SlashCommandBuilder()
@@ -16,17 +16,18 @@ export async function execute(interaction) {
     });
     return;
   }
-
+  const profile = new AttachmentBuilder("./assets/evernight_profile.png");
   const embed = new EmbedBuilder()
-    .setTitle("Profile")
-    .setDescription(`Welcome, **${interaction.user.username}**`)
+    .setTitle(`**${interaction.user.username}**'s`)
+    .setDescription(`Account Balance`)
     .addFields(
       { name: "Jades <:stellar_jade:1432377631210344530>", value: `${user.jades}`, inline: true },
       { name: "Credits <:credit:1432377745626759380>", value: `${user.credits}`, inline: true },
       { name: "Pity", value: `${user.pity}`, inline: true }
     )
     .setFooter({ text: `Registered at ${new Date(user.registeredAt).toLocaleString()}` })
-    .setColor("#a90000");
+    .setColor("#a90000")
+    .setThumbnail("attachment://evernight_profile.png");
 
-  await interaction.reply({ embeds: [embed] });
+  await interaction.reply({ embeds: [embed], files: [profile] });
 }
--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -2,19 +2,19 @@ import { REST, Routes } from "discord.js";
 import 'dotenv/config';
 import { command as register } from './commands/register.js';
 import { command as profile } from './commands/profile.js';
-import { command as bet} from './commands/bet.js';
+import { command as bet } from './commands/bet.js';
 import { command as daily } from './commands/daily.js';
+
 const commands = [register, profile, bet, daily].map(cmd => cmd.toJSON());
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 try {
-  console.log('🔄 Registering commands...');
+  console.log('Registering global commands...');
   await rest.put(
-    Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+    Routes.applicationCommands(process.env.CLIENT_ID), 
     { body: commands }
   );
-  console.log('Commands registered.');
+  console.log('✅ Global commands registered! (May take up to 1h to appear)');
 } catch (err) {
   console.error(err);
 }
-//auto generated
